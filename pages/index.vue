30e3cfa878a4fbe8922ae4e8762dc3c6fc52b5f1
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-03-12 14:55:08 +0200

    add sign in functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,56 @@
 <script setup>
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { ref } from "vue";
 definePageMeta({
   layout: "authentication",
 });
+
+const email = ref("");
+const password = ref("");
+
+
+const { $auth } = useNuxtApp();
+const { $db } = useNuxtApp();
+
+const login = async () => {
+  try {
+    const { user } = await signInWithEmailAndPassword(
+      $auth,
+      email.value,
+      password.value
+    );
+console.log(user)
+  } catch (error) {
+    console.log(error);
+  }
+};
 </script>
 
 <template>
   <div>
     <div class="flex flex-col justify-center items-center mt-9">
-      <img class="md:hidden mb-5 w-[120px]" src="~/assets/arabiameet-logo.png" alt="" />
+      <img
+        class="md:hidden mb-5 w-[120px]"
+        src="~/assets/arabiameet-logo.png"
+        alt=""
+      />
       <p class="text-2xl font-semibold">Sign in</p>
       <div class="mt-[50px]">
         <div
-          class="
-            flex
-            items-center
-            px-5
-            py-2
-            bg-gray-100
-            cursor-pointer
-            shadow-md
-            rounded-[8px]
-          "
+          class="flex items-center px-5 py-2 bg-gray-100 cursor-pointer shadow-md rounded-[8px]"
         >
           <img src="~/assets/google.svg" alt="" />
           <p class="text-black capitalize ml-3">continue with google</p>
         </div>
         <div
-          class="
-            flex
-            items-center
-            px-5
-            py-2
-            bg-blue-950
-            cursor-pointer
-            mt-2
-            shadow-md
-            rounded-[8px]
-          "
+          class="flex items-center px-5 py-2 bg-blue-950 cursor-pointer mt-2 shadow-md rounded-[8px]"
         >
           <img src="~/assets/facebook.svg" alt="" />
           <p class="text-white capitalize ml-3">continue with facebook</p>
         </div>
         <div
-          class="
-            flex
-            items-center
-            px-5
-            py-2
-            cursor-pointer
-            bg-black
-            mt-2
-            shadow-md
-            rounded-[8px]
-          "
+          class="flex items-center px-5 py-2 cursor-pointer bg-black mt-2 shadow-md rounded-[8px]"
         >
           <img src="~/assets/apple.svg" alt="" />
           <p class="text-white capitalize ml-3">continue with apple</p>
@@ -70,20 +68,12 @@ definePageMeta({
             >Email Address</label
           >
           <input
+          v-model="email"
             type="text"
             name="email"
             id="email"
             autocomplete="email"
-            class="
-              mt-2
-              shadow-sm
-              w-1/2
-              rounded
-              border-gray-500 border border-solid
-              focus:border-cyan-200
-              sm:text-sm
-              p-2
-            "
+            class="mt-2 shadow-sm w-1/2 rounded border-gray-500 border border-solid focus:border-cyan-200 sm:text-sm p-2"
           />
         </div>
 
@@ -92,32 +82,16 @@ definePageMeta({
             >Password</label
           >
           <input
+          v-model="password"
             type="password"
             name="password"
             id="password"
-            class="
-              mt-2
-              shadow-sm
-              w-1/2
-              rounded
-              border-gray-500 border border-solid
-              focus:border-cyan-700
-              sm:text-sm
-              p-2
-            "
+            class="mt-2 shadow-sm w-1/2 rounded border-gray-500 border border-solid focus:border-cyan-700 sm:text-sm p-2"
           />
         </div>
-        <div class="flex justify-center mt-6">
+        <div @click="login" class="flex justify-center mt-6">
           <p
-            class="
-              bg-cyan-700
-              px-6
-              py-2
-              cursor-pointer
-              font-bold
-              text-white
-              rounded-full
-            "
+            class="bg-cyan-700 px-6 py-2 cursor-pointer font-bold text-white rounded-full"
           >
             Login
           </p>
