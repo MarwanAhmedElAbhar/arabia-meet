49c347dc89f83383bda25c62c86e57f43c966121
MarwanAhmedElAbhar <marwan.a.elabhar@gmail.com>
2023-03-16 13:20:11 +0200

    add error block component

---
+++ components/ErrorBlock.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+    message: String,
+})
+
+</script>
+
+ <template>
+ <div>
+  <span class="bg-rose-100 shadow-md rounded px-4 py-2 text-red-600">{{ message }}</span>
+  </div>
+</template>
+
+
+
